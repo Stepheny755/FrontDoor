4874f713916472d8c1dad1bada179361f6e6610b
Stephen Yang <stepheny755@gmail.com>
2020-08-30 14:11:08 -0400

    updated wc_idle

--- webcam/wc_idle.py
+++ webcam/wc_idle.py
@@ -7,3 +7,11 @@ class Idle():
     def __init__(cam):
         self.stream = IOBytes()
         self.camera = cam
+
+    def check_act(self):
+        cam.capture(self.stream,"jpeg")
+        print(self.stream)
+        pass
+
+    def get_difference(self,i1,i2):
+        pass
