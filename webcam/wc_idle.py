acebdee01ef446fcb2583ccd7dd2318d7903cf0e
Stepheny755 <stepheny755@gmail.com>
2020-08-30 14:56:40 -0400

    updated wc_idle.py to include capture function

--- webcam/wc_idle.py
+++ webcam/wc_idle.py
@@ -1,6 +1,10 @@
-from picamera import PiCamera
+
 from io import BytesIO
+import numpy as np
+import picamera
+import picamera.array
 import time
+import cv2
 
 class Idle():
 
@@ -9,16 +13,29 @@ class Idle():
         self.camera = cam
 
     def check_activity(self):
+        pass
+
+    def get_image_capture_jpg(self):
         cam.capture(self.stream,"jpeg")
         print(self.stream)
-        data = np.fromstring(stream.getvalue(),dtype=np.uint8)
+        data = np.fromstring(self.stream.getvalue(),dtype=np.uint8)
+        image = cv2.imdecode(data,1)
+        image = image[:,:,::-1]
         print(data.shape)
-        pass
+        print(image.shape)
+        return image
+
+    def get_image_capture_bgr(self):
+        with picamera.array.PiRGBArray(self.camera) as stream:
+            self.camera.capture(stream,"bgr")
+            image = stream.array[:,:,::-1]
+            print(image.shape)
+            return image
 
     def get_difference(self,i1,i2):
         pass
 
 if(__name__=="__main__"):
-    with PiCamera() as cam:
+    with picamera.PiCamera() as cam:
         i = Idle(cam)
-        i.check_activity()
+        i.get_image_capture_bgr()
