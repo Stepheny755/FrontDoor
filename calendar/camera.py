537ebbe2579a66cc8198b3948d81d014cf56f916
Stephen Yang <stephy.yang@mail.utoronto.ca>
2021-09-19 00:48:54 -0400

    added motion detection to camera.py

--- calendar/camera.py
+++ calendar/camera.py
@@ -12,16 +12,38 @@ class Camera():
 
 	def __init__(self,def_ref_rate:int=5,def_timeout:int=30):
 		# internal parameters
-		self.ref_rate = def_ref_rate
+		self.passive_ref_rate = def_ref_rate
 		self.timeout = def_timeout
 
 		# capture content from video camera
 		self.vc = cv2.VideoCapture(0)
 
+        self.detection_baseline = None
+
 	def __del__(self):
 		self.vc.release()
-
 
 	def show_image(self):
 		success, img = self.vc.read()
 		cv2.imshow("Capture",img)
+
+    def detect_motion(self):
+        success, img = self.vc.read()
+        frame = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        frame = cv2.GaussianBlur(frame,(25,25),0)
+
+        # exit if camera recently initialized
+        if(self.detection_baseline==None):
+            self.detection_baseline = frame
+            return
+
+        delta = cv2.absdiff(detection_baseline,frame)
+        filtered = cv2.threshold(delta,50,255,cv2.THRESH_BINARY)
+
+        cv2.findContours(filtered,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            if cv2.contourArea(contour) < 5000:
+                continue
+            (x, y, w, h)=cv2.boundingRect(contour)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 1)
