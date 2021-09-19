0208e18a0d8cf4c2e37773fd3dd616aefcbdfef9
Stephen Yang <stephy.yang@mail.utoronto.ca>
2021-09-19 00:57:11 -0400

    fixed indenting and ran autopep8

--- calendar/camera.py
+++ calendar/camera.py
@@ -2,30 +2,24 @@ import cv2
 
 vc = cv2.VideoCapture(0)
 
-while True:
-	success, img = vc.read()
-	cv2.imshow("Video",img)
-	if cv2.waitKey(30) & 0xFF==ord("q"):
-		break
-
 class Camera():
 
-	def __init__(self,def_ref_rate:int=5,def_timeout:int=30):
-		# internal parameters
-		self.passive_ref_rate = def_ref_rate
-		self.timeout = def_timeout
+    def __init__(self, def_ref_rate: int = 5, def_timeout: int = 30):
+        # internal parameters
+        self.passive_ref_rate = def_ref_rate
+        self.timeout = def_timeout
 
-		# capture content from video camera
-		self.vc = cv2.VideoCapture(0)
+        # capture content from video camera
+        self.vc = cv2.VideoCapture(0)
 
         self.detection_baseline = None
 
-	def __del__(self):
-		self.vc.release()
+    def __del__(self):
+        self.vc.release()
 
-	def show_image(self):
-		success, img = self.vc.read()
-		cv2.imshow("Capture",img)
+    def show_image(self):
+        success, img = self.vc.read()
+        cv2.imshow("Capture", img)
 
     def detect_motion(self):
         success, img = self.vc.read()
