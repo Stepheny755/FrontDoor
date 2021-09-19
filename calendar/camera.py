8efa3615cca2ece3614a18f6088c8a8bdee2a2d4
Stephen <stephy.yang@mail.utoronto.ca>
2021-09-19 11:23:00 -0400

    working bounding box object code

--- calendar/camera.py
+++ calendar/camera.py
@@ -1,3 +1,4 @@
+import numpy as np
 import cv2
 
 class Camera():
@@ -7,10 +8,16 @@ class Camera():
         self.passive_ref_rate = def_ref_rate
         self.timeout = def_timeout
 
+        # camera resolution
+        self.res_w = 640
+        self.res_h = 480
+
         # capture content from video camera
         self.vc = cv2.VideoCapture(0)
 
-        self.detection_baseline = None
+        _, self.detection_baseline = self.vc.read()
+        self.detection_baseline = cv2.cvtColor(self.detection_baseline,cv2.COLOR_BGR2GRAY)
+        self.detection_baseline = cv2.GaussianBlur(self.detection_baseline,(25,25),0)
 
     def __del__(self):
         self.vc.release()
@@ -25,26 +32,38 @@ class Camera():
         frame = cv2.GaussianBlur(frame,(25,25),0)
 
         # exit if camera recently initialized
-        if(self.detection_baseline==None):
-            self.detection_baseline = frame
-            return
+        #if(self.detection_baseline==None):
+        #    self.detection_baseline = frame
+        #    return
+        #print((frame.shape))
+        #print((self.detection_baseline.shape))
 
-        delta = cv2.absdiff(detection_baseline,frame)
-        filtered = cv2.threshold(delta,50,255,cv2.THRESH_BINARY)
+        delta = cv2.absdiff(self.detection_baseline,frame)
+        filtered = cv2.threshold(delta,50,255,cv2.THRESH_BINARY)[1]
 
-        cv2.findContours(filtered,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        contours = cv2.findContours(filtered,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)[0]
 
-        for contour in contours:
-            if cv2.contourArea(contour) < 5000:
-                continue
-            (x, y, w, h)=cv2.boundingRect(contour)
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 1)
+        if(contours is not None):
+            for contour in contours:
+                if cv2.contourArea(contour) < 5000:
+                    continue
+                (x, y, w, h)=cv2.boundingRect(contour)
+                cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 1)
+
+        self.detection_baseline = frame
 
         cv2.imshow("Objects",frame)
 
         return
 
+    def get_baseline(self):
+        return self.detection_baseline
+
 if(__name__=="__main__"):
     c = Camera()
     while True:
         c.detect_motion()
+        cv2.imshow("Baseline",c.get_baseline())
+        key = cv2.waitKey(30)
+        if(key==ord("q")):
+            break
